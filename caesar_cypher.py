2cc79abc0c1ea7f6b6105a6e4bcced665183e073
Lee-TB <86998194+Lee-TB@users.noreply.github.com>
2021-09-21 10:59:23 +0700

    caesar cypher

---
+++ caesar_cypher.py
@@ -0,0 +1,24 @@
+def encrypt(text, k):
+    text = text.replace(" ", "")
+    new_text = list()
+    len_text = len(text)
+    for i in range(len_text):
+        if(text.isupper()):
+            new_text.append(chr(((ord(text[i]) + k - 65) % 26) + 65))
+        else:
+            new_text.append(chr(((ord(text[i]) + k - 97) % 26) + 97))
+    new_text = ''.join(new_text)
+    return new_text
+
+
+def decrypt(text, k):
+    text = text.replace(" ", "")
+    new_text = list()
+    len_text = len(text)
+    for i in range(len_text):
+        if(text.isupper()):
+            new_text.append(chr(((ord(text[i]) - k - 65) % 26) + 65))
+        else:
+            new_text.append(chr(((ord(text[i]) - k - 97) % 26) + 97))
+    new_text = ''.join(new_text)
+    return new_text
--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import reverse_cypher as rv
+import caesar_cypher as ca
 
 if __name__=='__main__':
-    print(rv.encrypt('reverse cypher'))
+    text = 'reverse cypher'
+    encrypt = rv.encrypt(text)
+    decrypt = rv.decrypt(encrypt)
+    print('reverse cypher: encrypt "{}", decrypt "{}"'.format(encrypt, decrypt))
+
+    print()
+
+    text = 'caesar cypher'
+    encrypt = ca.encrypt(text, 3)
+    decrypt = ca.decrypt(encrypt, 3)
+    print('caesar cypher: encrypt "{}", decrypt "{}"'.format(encrypt, decrypt))
     
